ea10598a45d5be1b3ff232f24c3926f5e60e68dc
https://github.com/arshadraza118 <arraza0323@gmail.com>
2024-03-07 17:15:48 +0500

    minor changes

--- Q1.ts
+++ Q1.ts
@@ -1,29 +1,29 @@
-// question no 1
+// question no 2
 let Name: string = "Arshad";
 console.log(`Hello ${Name}, would you like to learn some Python today?`);
 
-// question no 2
+// question no 3
 console.log(Name.toUpperCase());
 console.log(Name.toLowerCase());
 // console.log(Name.t);
 
-// Question no.3
+// Question no.4
 let quote: string = "I don't believe in quotes, because my life depends upon me not in other's wording";
 let author: string = "Arshad Raza";
 
-// Question no.3
+// Question no.5
 
 
 let famous_person: string = "Winston Churchill";
 let message: string = `${famous_person} once said, "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel."`;
 console.log(message);
 
-// Question no.3
+// Question no.6
 
 let formattedString: string = "First line\tTab\nThird line";
 console.log(formattedString);
 
-// Question no.3
+// Question no.7
 
 // Addition
 console.log("Addition:");
@@ -48,14 +48,14 @@ console.log(10 - 2);
 console.log(4 * 2);
 console.log(16 / 2);
 
-// Question no.3
+// Question no.8
 
 let favoriteNumber: number = 7;
 let message2: string = `My favorite number is ${favoriteNumber}.`;
 
 console.log(message2);
 
-// Question no.3
+// Question no.9
 // programming for fun
 
 /* Name: Arshad Raza
@@ -68,7 +68,7 @@ else {
     console.log("I am failed");
 }
 
-// Question no.3
+// Question no.10
 
 let friends: string[] = ["Asif", "Ali", "Akmal", "Navaid"];
 console.log(friends[0]);
@@ -77,12 +77,12 @@ console.log(friends[2]);
 console.log(friends[3]);
 
 
-// Question no.3
+// Question no.11
 let friends1: string[] = ["Asif", "Ali", "Akmal", "Navaid"];
 let greeting: string = "hello ,"
 for (let names of friends1) { console.log(greeting + names) };
 
-// Question no.3
+// Question no.12
 let favTransport: string[] = ["bike", "car", "aeroplane"];
 let favState: string[] = ["I like to ride ", "I will buy a ", "i will fly in "];
 
@@ -90,20 +90,20 @@ console.log(favState[0] + favTransport[0]);
 console.log(favState[1] + favTransport[1]);
 console.log(favState[2] + favTransport[2]);
 
-// Question no.3
+// Question no.13
 
 let guestName: string[] = ["Asif", "Ali", "Akmal", "Navaid"];
 for (let guest in guestName) {
     console.log(`Dear ${guestName[guest]}, you are invited for dinner.`);
 }
-// Question no.3
+// Question no.14
 guestName.pop();
 guestName.push("arshad");
 console.log(guestName);
 
 for (let guest in guestName) { console.log(`Dear ${guestName[guest]}, you are invited for dinner.`) };
 
-// Question no.3
+// Question no.15
 // Inform about the bigger dinner table
 console.log("\nGood news! We found a bigger dinner table.");
 
@@ -178,61 +178,63 @@ console.log("\nAlphabetical Order (sorted):");
 visitPlaces.sort();
 console.log(visitPlaces);
 
-// Question no.3
+// Question no.16
 console.log("Number of places in list:", visitPlaces.length);
 
-// Question no.3
-let mountain : string[]= ["Everest","K2","Lhotse","Manaslu"];
+// Question no.17
+let mountain: string[] = ["Everest", "K2", "Lhotse", "Manaslu"];
 console.log("\nList of Mountains");
 console.log(mountain);
 
-// Question no.3
+// Question no.18
 //data of student 1
 
-let student :{Name:string , Class:number, age:number, attendenceOfToday:boolean} =
-{Name:"arshad",
-Class:14,
-age:22,
-attendenceOfToday:true,
+let student: { Name: string, Class: number, age: number, attendenceOfToday: boolean } =
+{
+    Name: "arshad",
+    Class: 14,
+    age: 22,
+    attendenceOfToday: true,
 };
 //data of student 2
-let student1 :{Name:string , Class:number, age:number, attendenceOfToday:boolean} =
-{Name:"asif",
-Class:12,
-age:22,
-attendenceOfToday:false,
+let student1: { Name: string, Class: number, age: number, attendenceOfToday: boolean } =
+{
+    Name: "asif",
+    Class: 12,
+    age: 22,
+    attendenceOfToday: false,
 };
 
 console.log(student);
 console.log(student1);
 
-// Question no.3
+// Question no.19
 let numbers: number[] = [1, 2, 3, 4, 5];
 
 // Accessing an element at an out-of-bounds index
 console.log(numbers[10]);
-  // Trying to access element at index 10, which doesn't exist
+// Trying to access element at index 10, which doesn't exist
 
 
 // Accessing an element at a valid index
 console.log(numbers[2]); // Accessing element at index 2, which exists
 
-// Question no.3
-let student2 : string= "present"
+// Question no.20
+let student2: string = "present"
 console.log("Is student2== present?I pridicted true.");
-console.log(student2== "present");
+console.log(student2 == "present");
 
-let nextBallRun : number =6;
+let nextBallRun: number = 6;
 console.log("will nextBallRun == 6? I pridict true");
-console.log(nextBallRun==6);
+console.log(nextBallRun == 6);
 
-let tommorow : string = "monday";
+let tommorow: string = "monday";
 console.log("Is tommorow== monday?I pridicted true.");
-console.log(tommorow=="monday");
+console.log(tommorow == "monday");
 
-let attendClass : boolean= true;
+let attendClass: boolean = true;
 console.log("will I attendclass? I pridicted true");
-console.log(attendClass==true);
+console.log(attendClass == true);
 
 
 
